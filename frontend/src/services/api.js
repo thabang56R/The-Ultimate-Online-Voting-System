e05d56eeeb22f8a3c0bc540dd13b35d0b1fbbbb3
thabang56R <thabang56@hotmail.com>
2026-03-23 07:42:20 +0800

    change vite from local to vercel backend api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-/* Attach JWT token if exists */
+
 API.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
